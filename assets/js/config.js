4c14f8fa543b1897a20453dc354b41bc7c95f1a5
kevinmesiab <kmesiab@gmail.com>
2015-07-09 14:44:05 -0700

    factored out login ux config into config file, added smartsheet app secret and client id

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,33 @@
 var config = config || {};
 
+config.smartsheet = {
+    
+    clientId : "",
+    appSecret : ""
+    
+}
+
 config.database = {
     name : "sql383324",
     user : "",
     pass : "",
     port : 3306,
     host : "sql3.mysqlhosting.net"
+}
+
+config.login = {
+    
+    title : "Let's Get Started!",
+    text  : "You must first sign in to your <strong>smartsheet</strong> account",
+    type  : "info",
+    html  : true,
+    
+    showConfirmButton : true,
+    confirmButtonText : "Sign In",
+    
+    showCancelButton  : true,
+    cancelButtonText  : "Nevermind",
+    
+    closeOnConfirm    : false,
+    closeOnCancel     : false,    
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,7 @@ var app = app || {};
 
 app.login = function() {
     
-    swal({
-        title : "Let's Get Started!",
-        text  : "You must first sign in to your <strong>smartsheet</strong> account",
-        type  : "info",
-        html  : true,
-        
-        showConfirmButton : true,
-        confirmButtonText : "Sign In",
-        
-        showCancelButton  : true,
-        cancelButtonText  : "Nevermind",
-        
-        closeOnConfirm    : false,
-        closeOnCancel     : false,
-        //imageUrl          : "http://lh3.googleusercontent.com/yQ3iHbrAsf2gmHVr9r8SE_XdIKDEJ4KJdyrqDMPIPbBmwehFW81e0M1SSRjmArPz0Cah3W6nDC5v6BPf=s120?gr=14285c03a63"
-    },
+    swal(config.login),
     
     function(isConfirm){
         
