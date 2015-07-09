a36fb4908bc98c66f11cd85e8ec8cf8042489ae3
kevinmesiab <kmesiab@gmail.com>
2015-07-09 14:11:10 -0700

    modularized the login ux behavior

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,42 @@
-swal({
-    title: "Let's Get Started!",
-    text: "You must first sign in to your <strong>smartsheet</strong> account",
-    type : "info",
-    showConfirmButton: false,
-    html: true,
-    showConfirmButton : true,
-    confirmButtonText : "Sign In",
-    showCancelButton : true,
-    cancelButtonText : "Nevermind",
-    //imageUrl: "http://lh3.googleusercontent.com/yQ3iHbrAsf2gmHVr9r8SE_XdIKDEJ4KJdyrqDMPIPbBmwehFW81e0M1SSRjmArPz0Cah3W6nDC5v6BPf=s120?gr=14285c03a63"
-});
+var app = app || {};
+
+app.login = function() {
+    
+    swal({
+        title : "Let's Get Started!",
+        text  : "You must first sign in to your <strong>smartsheet</strong> account",
+        type  : "info",
+        html  : true,
+        
+        showConfirmButton : true,
+        confirmButtonText : "Sign In",
+        
+        showCancelButton  : true,
+        cancelButtonText  : "Nevermind",
+        
+        closeOnConfirm    : false,
+        closeOnCancel     : false,
+        //imageUrl          : "http://lh3.googleusercontent.com/yQ3iHbrAsf2gmHVr9r8SE_XdIKDEJ4KJdyrqDMPIPbBmwehFW81e0M1SSRjmArPz0Cah3W6nDC5v6BPf=s120?gr=14285c03a63"
+    },
+    
+    function(isConfirm){
+        
+        if (isConfirm) {
+            
+            swal("Success!", "One moment while we set up the awesome!", "success");
+        
+        } else {
+        
+            swal("Cancelled", "You have to be logged in to use SmartView", "error");
+        }
+        
+        return isConfirm;
+        
+    });
+    
+};
+
+
+if( ! app.login() ) {
+    app.login();
+};
