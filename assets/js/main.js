d7daa2b6b1a797f6e6b0bbb5dc5d92275d3f9783
kevinmesiab <kmesiab@gmail.com>
2015-07-09 12:54:29 -0700

    Initial Github Pages commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+swal({
+    title: "Let's Get Started!",
+    text: "You must first sign in to your <strong>smartsheet</strong> account",
+    type : "info",
+    showConfirmButton: false,
+    html: true,
+    showConfirmButton : true,
+    confirmButtonText : "Sign In",
+    showCancelButton : true,
+    cancelButtonText : "Nevermind",
+    //imageUrl: "http://lh3.googleusercontent.com/yQ3iHbrAsf2gmHVr9r8SE_XdIKDEJ4KJdyrqDMPIPbBmwehFW81e0M1SSRjmArPz0Cah3W6nDC5v6BPf=s120?gr=14285c03a63"
+});
